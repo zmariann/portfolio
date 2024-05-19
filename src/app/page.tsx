be99ce6dc43e4685d276125ea76ebe9c277080a4
zmariann <zaszlosmariann@gmail.com>
2024-05-19 13:00:08 +0200

    version 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,114 @@
-import Image from 'next/image'
+import Link from "next/link";
 
-export default function Home() {
+const importantWords = [
+  {
+    word: "MULTIMEDIA CONTENT ",
+    href: "/about",
+  },
+  {
+    word: "PHOTOGRAPHY ",
+    href: "/keyWords/photo",
+  },
+  {
+    word: "CODING STUFF",
+    href: "/projects",
+  },
+  {
+    word: "CREATING WORLDS ",
+    href: "/contact",
+  },
+  {
+    word: "ADOBE SOFTWARES ",
+    href: "/contact",
+  },
+  {
+    word: "PERSONALISED DESIGN*",
+    href: "/contact",
+  },
+];
+
+export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div className="flex justify-center items-center flex-col">
+        <div className="md:pl-[0px] md:pr-[0px] mt-[25%] md:mt-[15%] mb-[10%] md:mb-[15%] ml-2">
+          <div className="title">PORTFOLIO</div>
+
+          <div className="display-linebreak">
+            UNIQUE MULTIMEDIA PROJECTS {"\n"}
+            CREATED BY MARIANN ZÁSZLÓS
+          </div>
+
+          <Link className="underline" href="/about">
+            READ MORE
+          </Link>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="leading-[10vw] md:leading-[14vh] leading-[12vh] flex flex-col">
+        {importantWords.map((d, i) => (
+          <Link className="effraBold text-[10vw] text-[15vh] md:text-8xl hover:bg-[#e1cb9c]" key={i} href={d.href}>
+            {d.word}
+          </Link>
+        ))}
+      </div>
+
+      <div className="">
+        *CLICK ON THESE WORDS TO GET THE DETAILS
+      </div>
+
+      <div className="md:hidden">
+        <img
+          className="w-full h-auto"
+          src="weblap.jpg"
+          alt="skydive"
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="md:flex hidden">
+        <img
+          className="md:w-[50%] w-[90%] h-auto"
+          src="weblap.jpg"
+          alt="skydive"
+        />
+        <img
+          className="md:w-[50%] w-[90%] h-auto"
+          src="weblap.jpg"
+          alt="skydive"
+        />
       </div>
-    </main>
-  )
+
+      <div className="flex justify-center items-center my-[5%]">
+        <div className="text-sm md:text-base">
+          <p>MARIANN ZÁSZLÓS</p>
+          <p>INFO@ZMARIANN.COM</p>
+        </div>
+      </div>
+
+      <div className="border-t-[3px] border-mainBlack">
+        <div className="hidden md:flex ml-5">
+          <div className="mt-5 mb-3 ml-5 mr-4">LINKEDIN</div>
+          <div className="mt-5 mb-3 ml-5 mr-4">GITHUB</div>
+        </div>
+
+        <div className="flex md:hidden px-4 pb-3 pt-2 gap-4">
+          <a
+            href="https://www.linkedin.com/in/zmariann/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="linkedin.png" alt="github" />
+          </a>
+          <img src="github.png" alt="linkedin" />
+        </div>
+      </div>
+    </div>
+  );
 }
+
+
+// TYPESCRIPT REACT NEXT.JS TAILWIND GIT NODE.JS PHOTOSHOP LIGTHROOM
+// PREMIER PRO CONTENT AUDIOVISUAL CODING CREATING WORLDS*
+// unusual solutions yes, I consider coding a creative activity.
+// I'm enjoying to use these fantastic tools and I'm highly willing to learn something new time by time.
+// different tools how work together
