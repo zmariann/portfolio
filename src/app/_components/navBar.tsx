6441a7c712d2c7fd639cbfc71f91a208827d66cd
zmariann <zaszlosmariann@gmail.com>
2024-07-24 15:59:10 +0200

    finishing nav bar

--- src/app/_components/navBar.tsx
+++ src/app/_components/navBar.tsx
@@ -19,11 +19,6 @@ const navLinks = [
 
 export default function NavBar() {
   const [showMenu, setMenu] = useState(false);
-  const btnRef = useRef(null);
-
-  function toggleMenu() {
-    setMenu(!showMenu);
-  }
 
   useEffect(() => {
     const closeDropdown = (): void => {
@@ -35,56 +30,38 @@ export default function NavBar() {
 
   return (
     <div>
-      <div className="flexbox border-b-[3px] border-mainBlack bg-background fixed w-full justify-end md:justify-between md:pt-6 md:pb-4 md:px-8">
-        <div className="hidden md:flex">
-          <Link className="w-[85px] lg:w-28" href="/">
-          <img
-              src="logoNeue.png"
-              alt="logo"
-            /></Link>
-        </div>
+      <div className="hidden sm:flex sm:border-b-[3px] sm:border-mainBlack sm:bg-background sm:fixed sm:w-full sm:justify-between sm:pt-6 sm:pb-4 sm:px-8">
+        <Link className="w-[85px] lg:w-28" href="/">
+          <img src="logoNeue.png" alt="logo" />
+        </Link>
 
-        <div className="hidden md:flex gap-6">
+        <div className="flex gap-6">
           {navLinks.map((d, i) => (
             <Link className="lg:text-3xl lg:pt-2" key={i} href={d.href}>
               {d.label}
             </Link>
           ))}
         </div>
-        
-
       </div>
 
-      <button ref={btnRef} onClick={toggleMenu} className="md:hidden">
-        {showMenu ? (
-            <div className="">ZAMARKA</div>
-        ) : (
-          <div className="flex justify-end">
-            <img
-              className="w-[60%] mb-2 mr-1 mt-[7px] pr-1"
-              src="menu.png"
-              alt="menu"
-            />
-          </div>
-        )}
-      </button>
+      <div className="sm:hidden flex justify-center w-full border-b-[2px] border-mainBlack fixed bg-background">
+        <button onClick={() => setMenu(!showMenu)}>
+          <img className="w-[100px]" src="logoNeue.png" alt="logo" />
+        </button>
+      </div>
 
       {showMenu && (
-        <div className="fixed w-full mt-9 h-screen flex flex-col items-center bg-background md:hidden requires-no-scroll">
-          <section className="my-4 flex flex-col items-center gap-1">
-            <Link className="" href="/">
-              HOME
+        <div className="w-full h-screen fixed top-0 left-0 gap-2 bg-background requires-no-scroll flex flex-col items-center justify-center">
+          <Link className="" href="/">
+            HOME
+          </Link>
+          {navLinks.map((d, i) => (
+            <Link key={i} href={d.href}>
+              {d.label}
             </Link>
-            {navLinks.map((d, i) => (
-              <Link key={i} href={d.href}>
-                {d.label}
-              </Link>
-            ))}
-          </section>
+          ))}
         </div>
       )}
-
-
     </div>
   );
 }
