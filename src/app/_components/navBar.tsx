6d3343a572ee33b6a9643aacfce212bb5fac6bdd
zmariann <zaszlosmariann@gmail.com>
2024-07-25 13:32:21 +0200

    some responsive design changes

--- src/app/_components/navBar.tsx
+++ src/app/_components/navBar.tsx
@@ -31,13 +31,13 @@ export default function NavBar() {
   return (
     <div>
       <div className="hidden sm:flex sm:border-b-[3px] sm:border-mainBlack sm:bg-background sm:fixed sm:w-full sm:justify-between sm:pt-6 sm:pb-4 sm:px-8">
-        <Link className="w-[85px] lg:w-28" href="/">
+        <Link className="w-[100px] lg:w-28" href="/">
           <img src="logoNeue.png" alt="logo" />
         </Link>
 
         <div className="flex gap-6">
           {navLinks.map((d, i) => (
-            <Link className="lg:text-3xl lg:pt-2" key={i} href={d.href}>
+            <Link className="lg:text-3xl text-2xl lg:pt-2" key={i} href={d.href}>
               {d.label}
             </Link>
           ))}
@@ -46,17 +46,17 @@ export default function NavBar() {
 
       <div className="sm:hidden flex justify-center w-full border-b-[2px] border-mainBlack fixed bg-background">
         <button onClick={() => setMenu(!showMenu)}>
-          <img className="w-[100px]" src="logoNeue.png" alt="logo" />
+          <img className="w-[100px] sm:m-3 m-1" src="logoNeue.png" alt="logo" />
         </button>
       </div>
 
       {showMenu && (
         <div className="w-full h-screen fixed top-0 left-0 gap-2 bg-background requires-no-scroll flex flex-col items-center justify-center">
-          <Link className="" href="/">
+          <Link className="text-3xl" href="/">
             HOME
           </Link>
           {navLinks.map((d, i) => (
-            <Link key={i} href={d.href}>
+            <Link className="text-3xl" key={i} href={d.href}>
               {d.label}
             </Link>
           ))}
